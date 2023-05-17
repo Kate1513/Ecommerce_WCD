6f85bceb8b5edc3d18ed895d234df4e3871c1347
Kate1513 <kate_1513@hotmail.com>
2023-05-17 14:54:06 -0500

    v4 - Visualizar productos en pagina de inicio

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -9,13 +9,13 @@ function Productos() {
   }, [])
   return (
     <>
-      <div>
-        <ul>
+      <div className='flex justify-between'>
+        <ul className='flex flex-wrap justify-center'>
           {productos.map((producto) => (
-            <li key={producto.id}>
+            <li key={producto.id} className='m-4'>
               <div className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
                 <a href='#'>
-                  <img className='p-8 rounded-t-lg' src={producto.images[0]} alt='product image' />
+                  <img className='p-8 rounded-t-lg max-h-48' src={producto.images[0]} alt='product image' />
                 </a>
                 <div className='px-5 pb-5'>
                   <a href='#'>
@@ -30,7 +30,7 @@ function Productos() {
                       href='#'
                       className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
                     >
-                      Añadir al carrito
+                      Añadir
                     </a>
                   </div>
                 </div>
